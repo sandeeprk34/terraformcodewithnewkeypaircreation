a766845b149c8b2a972a397c730df8b3369d2684
sandeeprk34 <sandeeprk34@gmail.com>
2018-04-25 18:38:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ provider "aws" {
 resource "aws_key_pair" "mykeypair" {
   key_name   = "tfkey"
 
- public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCVUA0A9W6hwOhj1n+Fn0vQ6bDkkxWkQSr4xmNv86gyV/ZrVoWdFhwiCa0sqs1IJcrDv3FcC9lUYiaEiKC+I6BEUSk9gE9PimVYDjGoUliAqPZWPNu9VpbQ1XPOY52XbrWi70KLI1NKqLAmrnLWloy8l31R6U5cdLhkqWMFuzaaIVyJvQwNB3m2jDlLsYkwn4vfGXCR6yRfMATvG2T1D81m6nrdb5kvfuoSi8TiQPZ8Ortfdji8Q4v0tcRJ0pQTqa1CL+zWmmHdTTsfFieOoKJ6f3sI/ungpmGzSQTsnBtAjrOEvag7QDiiUVfm5QMm0qR4COjjnJjelF65Z8vx7543 ec2-user@ip-172-31-29-10.us-west-2.compute.internal"
+ #public_key = "ssh-rsasdfff sfsfssfsfsf+Ffsfsffn0vQ6bDkkxWkQSr4xmNv86gyV/ZrVoWdFhwiCa0sqs1IJcrDv3FcC9lUYiaEiKC+I6BEUSk9gE9PimVYDjGoUliAqPZWPNu9VpbQ1XPOY52XbrWi70KLI1NKqLAmrnLWloy8l31R6U5cdLhkqWMFuzaaIVyJvQwNB3m2jDlLsYkwn4vfGXCR6yRfMATvG2T1D81m6nrdb5kvfuoSi8TiQPZ8Ortfdji8Q4v0tcRJ0pQTqa1CL+zWmmHdTTsfFieOoKJ6f3sI/ungpmGzSQTsnBtAjrOEvag7QDiiUVfm5QMm0qR4COjjnJjelF65Z8vx7543 ec2-user@ip-172-31-29-10.us-west-2.compute.internal"
  #public_key = "${file("tfkey.pub")}"    #Here we are giving the file location of public key file instead of hardcoding the public key.
  }
 
