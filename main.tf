ad8cd117733937cd26a59f3af30149f60ef09709
sandeeprk34 <sandeeprk34@gmail.com>
2018-04-25 18:13:28 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,138 +3,6 @@ provider "aws" {
  region     = "${var.region}"
 }
 
-#####VPC Creation#####
-resource "aws_vpc" "vpc" {
-  cidr_block = "${var.vpccidr}"
- 
-  tags {
-    Name = "TerraTestVPC"
-  }
-}
-
-####Data Sources for AZs######
-
-#data "aws_availability_zone" "az1" {
-# name = "us-west-2a"
-#}
-
-#data "aws_availability_zone" "az2" {
-# name = "us-west-2b"
-#}
-
-#data "aws_availability_zones" "az" {
-# name = ["us-west-2a","us-west-2b"]
-#}
-
-
-
-#####Subnets Creation#####
-
-resource "aws_subnet" "pubsub1" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.pubsubnet1}"
-  map_public_ip_on_launch = true
-  #availability_zone = "${data.aws_availability_zone.az1.name}"
-  #availability_zone = "${data.aws_availability_zones.az.name[0]}"
-   availability_zone = "${var.zones[0]}"
-  tags {
-    Name = "TerraPubSub1"
-  }
-}
-
-resource "aws_subnet" "prisub1" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.prisubnet1}"
-  availability_zone = "${var.zones[0]}"
-
-  tags {
-    Name = "TerraPriSub1"
-  }
-}
-
-
-resource "aws_subnet" "pubsub2" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.pubsubnet2}"
-  map_public_ip_on_launch = true
-  availability_zone = "${var.zones[1]}"
-
-  tags {
-    Name = "TerraPubSub2"
-  }
-}
-
-resource "aws_subnet" "prisub2" {
-  vpc_id     = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.prisubnet2}"
-  availability_zone = "${var.zones[1]}"
-
-  tags {
-    Name = "TerraPriSub2"
-  }
-}
-
-####IGW#####
-resource "aws_internet_gateway" "gw" {
-  vpc_id = "${aws_vpc.vpc.id}"
-
-  tags {
-    Name = "TerraIGW"
-  }
-}
-
-######Route Table######
-
-resource "aws_route_table" "r" {
-  vpc_id = "${aws_vpc.vpc.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.gw.id}"
-  }
-
-  tags {
-    Name = "TerraRouteTable"
-  }
-}
-
-### Subnet Associations to Route Table####
-
-resource "aws_route_table_association" "a" {
-  route_table_id = "${aws_route_table.r.id}"
-  subnet_id      = "${aws_subnet.pubsub1.id}"
-  
-}
-
-resource "aws_route_table_association" "b" {
-  route_table_id = "${aws_route_table.r.id}"
-  subnet_id     = "${aws_subnet.pubsub2.id}"
-}
-
-
-#####Security Group######
-resource "aws_security_group" "sec" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-  vpc_id = "${aws_vpc.vpc.id}"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-}
-  tags {
-    Name = "TerraSecGrp"
-  }
-}
-
 ######Creating a new key pair while launching an EC2 instance######
        ##1.Generate Public and Private Keys
 
@@ -148,49 +16,13 @@ resource "aws_instance" "myins" {
     ami = "${lookup(var.images, var.region)}" 
     instance_type = "t2.micro"
     associate_public_ip_address = "true"
-    subnet_id = "${aws_subnet.pubsub1.id}"
     availability_zone = "${var.zones[0]}"
-    vpc_security_group_ids = ["${aws_security_group.sec.id}"] 
     tags {
         Name = "TestEC2"   
     }
     key_name = "${aws_key_pair.mykeypair.key_name}"
 }
 
-####Get the running EC2 Instances#####
-
-data "aws_instance" "insdata" {
-  instance_id = "${aws_instance.myins.id}"
-
-  filter {
-    name   = "instance-state-name"
-    values = ["running"]
-  }
-}
-
-
-######Creating an EBS Volume#####
-resource "aws_ebs_volume" "myebs" {
-  availability_zone = "us-west-2a"
-  size              = 1
-}
-
-
-
-####Attaching an EBS Volume to the EC2 instance#####
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdl"
-  volume_id   = "${aws_ebs_volume.myebs.id}"
-  instance_id = "${data.aws_instance.insdata.instance_id}"
-}
-
-
-####Attaching an EBS Volume to the EC2 instance#####
-#resource "aws_volume_attachment" "ebs_att" {
-#  device_name = "/dev/sdz"
- # volume_id   = "${aws_ebs_volume.myebs.id}"
-  #instance_id = "${aws_instance.myins.id}"
-#}
 
 
  
