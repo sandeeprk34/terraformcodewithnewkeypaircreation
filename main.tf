df1907d7932daf7c6bc1af5ca7cae9ed84524828
sandeeprk34 <sandeeprk34@gmail.com>
2018-04-25 21:19:10 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,6 +25,20 @@ resource "aws_instance" "myins" {
         Name = "TestEC2"   
     }
     key_name = "${aws_key_pair.mykeypair.key_name}"
+   #####For the user data 
+    # 1>I am directly writing the user data script here only as shown below
+   
+    #user_data= << EOF
+         #!# /bin/bash
+         #yum install -y httpd
+         #service httpd start
+         #chkconfig httpd on
+        # echo  "<h1>Terraform</h1>" > /var/www/html/index.html
+    #EOF
+  # 2>Or We can create file with th user data and give the file path using the file function.
+    #user_data =  "${file("installhttpd.sh")}"             
+ 
+    
 }
 
 
